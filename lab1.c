49eb75639d07273e4966175e4c08cbfa106da4eb
Francesc Moll <38578393+fmoll-upc@users.noreply.github.com>
2018-09-12 19:48:27 +0200

    Create lab1.c
    First version of C code for ESDC lab 1 (VGA)

---
+++ lab1.c
@@ -0,0 +1,57 @@
+/*
+ * lab1.c
+ *
+ *  Created on: 12/09/2018
+ *      Author: francesc.moll
+*/
+
+
+
+#include "xparameters.h"
+#include "xgpio.h"
+
+
+//====================================================
+
+int main (void)
+{
+
+// Pointer and variable declaration
+    XGpio xpos, push, leds;
+	int i, psb_check;
+
+    xil_printf("-- Start of the Program --\r\n");
+
+// Pointer initialization
+// Check out your own XPAR ID symbol name declared in xparameters.h
+// The format is always XPAR_<NAME_IN_VIVADO>_DEVICE_ID
+
+
+	XGpio_Initialize(&push, XPAR_BUTTONS_DEVICE_ID);
+	XGpio_SetDataDirection(&push, 1, 0xffffffff); //input
+
+	XGpio_Initialize(&leds, XPAR_LEDS_DEVICE_ID);
+	XGpio_SetDataDirection(&leds, 1, 0x00000000); //output
+
+	XGpio_Initialize(&leds, XPAR_XPOS_DEVICE_ID);
+	XGpio_SetDataDirection(&xpos, 1, 0x00000000); //output
+
+	while (1)
+	{
+// Read value of buttons
+	  psb_check = XGpio_DiscreteRead(&push, 1);
+
+	  xil_printf("Push Buttons Status %x\r\n", psb_check);
+
+
+// Write value of buttons to leds (example)
+	  XGpio_DiscreteWrite(&leds, 1, psb_check);
+
+// Write value of buttons to xpos (example)
+	  XGpio_DiscreteWrite(&xpos, 1, psb_check);
+
+	  // Waiting cycles
+	  for (i=0; i<9999999; i++);
+	}
+
+}
